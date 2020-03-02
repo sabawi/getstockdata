30df0cc4756b355f8f980b479bccea63fd7db739
Al Sabawi <sabawi@gmail.com>
2020-03-02 17:46:11 -0500

    Many upgrades and function enhancements

--- stockdatalib.py
+++ stockdatalib.py
@@ -11,20 +11,20 @@ sp_cons_csv = ''
 fund_filename = ''
 xcolumn_name = 'date'
 column_map = {}
-
+sp_df = ''
 
 def set_stock(s):
     global stock
     global directory
     global sp_cons_csv
     global fund_filename
+    global sp_df
 
     if (directory == ''):
         raise NameError("Error: 'data directory' is NOT set")
         print('ERROR')
     else:
-        stock = s
-        sp_cons_csv = directory + 'sp_const.csv'
+        stock = s.lower()
         sp_df = pd.read_csv(sp_cons_csv)
         if stock.upper() not in sp_df['Symbol'].values:
             raise NameError("Error: '" + stock + "' is NOT AVAILABLE")
@@ -34,15 +34,23 @@ def set_stock(s):
 def get_stock():
     return stock
 
+def get_stock_info(symbol):
+    global sp_df
+
+    # Get stock company information
+    stock_info = sp_df[sp_df.Symbol == symbol.upper()]
+    company_name = stock_info['Name'].iloc[0]
+    company_sector = stock_info['Sector'].iloc[0]
+    return {'stock' : [stock], 'name': [company_name], 'sector':[company_sector]}
 
 def set_data_directory(datadir):
     global directory
+    global sp_cons_csv
     directory = datadir
-
+    sp_cons_csv = directory + 'sp_const.csv'
 
 def parser(x):
     return datetime.datetime.strptime(x, "%Y-%m-%d")
-
 
 def days_count(from_date, to_date=datetime.datetime.today().date()):
     if isinstance(from_date, datetime.datetime) is False:
@@ -72,10 +80,12 @@ def time_series_trendline(datetime_axis, values):
 
 
 def GetSP500_List():
+    global sp_cons_csv
     sp_df = pd.DataFrame()
     try:
         sp_df = pd.read_csv(sp_cons_csv)
         sp_df.sort_values('Symbol', ascending=True, inplace=True)
+
     except:
         print("Cannot open file", sp_cons_csv)
 
@@ -107,7 +117,8 @@ def GetStockDataFrame(symbol):
     return df
 
 
-def GetFund_Dict(stock):
+def GetFund_Dict(symbol):
+    set_stock(symbol)
     global column_map
     sources = ['income-statement', 'cash-flow-statement', 'balance-sheet-statement',
                'enterprise-value', 'company-key-metrics', 'financial-ratios']
@@ -143,7 +154,7 @@ def GetFund_Dict(stock):
     return fund_df_list.keys(), column_map, fund_df_list
 
 
-def where_is_column(name):
+def FindColumn(name):
     global column_map
     col_names = [key for key, value in column_map.items() if name.lower() in key.lower()]
     dataframes = [value for key, value in column_map.items() if key in col_names]
@@ -174,7 +185,7 @@ def CleanUp_DataFrame(main_df):
     return main_df
 
 
-def df_start_after_datetime(df, start, end=datetime.datetime.today()):
+def DatesRange(df, start, end=datetime.datetime.today()):
     return df[(df.index >= start) & (df.index <= end)].copy()
 
 
@@ -183,16 +194,13 @@ def skip_every_n(df, n):
     return df2
 
 
-def plot_basic_charts():
-    if (stock == ""):
-        print('Error: Stock Symbol is NOT set.')
-        return
+def plot_basic_charts(symbol):
+    set_stock(symbol)
 
+    stock_info = get_stock_info(symbol)
     # Get stock company information
-    sp_df = pd.read_csv(sp_cons_csv)
-    stock_info = sp_df[sp_df.Symbol == stock.upper()]
-    company_name = stock_info['Name'].iloc[0]
-    company_sector = stock_info['Sector'].iloc[0]
+    company_name = stock_info['name'][0]
+    company_sector = stock_info['sector'][0]
 
     stock_title = stock.upper() + ' ' + '(' + company_name + ')' + ' [' + company_sector + ' Sector]'
     price_df = GetStockDataFrame(stock)
@@ -235,12 +243,13 @@ def plot_basic_charts():
 
     fig = plt.figure(figsize=(15, 6))
 
+    linear_trends_df = pd.DataFrame()
     # ***************************************************
     # Find the full trend line
     x = range(len(price_df['Timestamps']))
     fit = np.polyfit(x, price_df['AdjClose'].astype(float), 1)
     fit_fn = np.poly1d(fit)
-    print('5 Years Trend Line Function :' + str(fit_fn))
+    linear_trends_df['5 Years Trend Line Function'] = [str(fit_fn).strip()]
 
     trend = []
     for k in x:
@@ -256,7 +265,7 @@ def plot_basic_charts():
     x = range(len(price_df_2y['Timestamps']))
     fit = np.polyfit(x, price_df_2y['AdjClose'].astype(float), 1)
     fit_fn = np.poly1d(fit)
-    print('2 Years Trend Line Function :' + str(fit_fn))
+    linear_trends_df['2 Years Trend Line Function'] = [str(fit_fn).strip()]
 
     trend = []
     for k in x:
@@ -271,7 +280,7 @@ def plot_basic_charts():
     x = range(len(price_df_1y['Timestamps']))
     fit = np.polyfit(x, price_df_1y['AdjClose'].astype(float), 1)
     fit_fn = np.poly1d(fit)
-    print('1 Year Trend Line Function :' + str(fit_fn))
+    linear_trends_df['1 Year Trend Line Function'] = [str(fit_fn).strip()]
 
     trend = []
     for k in x:
@@ -286,7 +295,7 @@ def plot_basic_charts():
     x = range(len(price_df_6m['Timestamps']))
     fit = np.polyfit(x, price_df_6m['AdjClose'].astype(float), 1)
     fit_fn = np.poly1d(fit)
-    print('6 Months Trend Line Function :' + str(fit_fn))
+    linear_trends_df['6 Months Trend Line Function'] = [str(fit_fn).strip()]
 
     trend = []
     for k in x:
@@ -314,10 +323,10 @@ def plot_basic_charts():
     y_list.append('5 Yr Trend')
 
     price_df.plot(ax=axs[0], x='Timestamps', y=y_list,
-                  title=stock_title)
-    price_df_2y.plot(ax=axs[0], x='Timestamps', y=['2 Yr Trend'])
-    price_df_1y.plot(ax=axs[0], x='Timestamps', y=['1 Yr Trend'])
-    price_df_6m.plot(ax=axs[0], x='Timestamps', y=['6 Month Trend'], grid=True)
+                  title=stock_title, lw=2)
+    price_df_2y.plot(ax=axs[0], x='Timestamps', y=['2 Yr Trend'], lw=2)
+    price_df_1y.plot(ax=axs[0], x='Timestamps', y=['1 Yr Trend'], lw=2)
+    price_df_6m.plot(ax=axs[0], x='Timestamps', y=['6 Month Trend'], grid=True, lw=2)
 
     axs[0].set_ylabel('Stock Price')
 
@@ -362,18 +371,18 @@ def plot_basic_charts():
 
     plots_df = pd.DataFrame(dic).set_index('Timestamps').dropna()
 
-    plots_df.plot(ax=axs[1], y=['EPS'], grid=True)
-    plots_df.plot(ax=axs[1], y=['FCF/Share'], grid=True)
+    plots_df.plot(ax=axs[1], y=['EPS'], grid=True, lw=4)
+    plots_df.plot(ax=axs[1], y=['FCF/Share'], grid=True, lw=4)
     axs[1].set_ylabel('Earning && FCF/Share')
-    plots_df.plot(ax=axs[2], y=['P/E'], grid=True)
-    plots_df.plot(ax=axs[2], y=['Calc. P/E'], grid=True)
+    plots_df.plot(ax=axs[2], y=['P/E'], grid=True, lw=4)
+    plots_df.plot(ax=axs[2], y=['Calc. P/E'], grid=True, lw=4)
     axs[2].set_ylabel('Price/Earning (P/E)')
 
-    plots_df.plot(ax=axs[3], y=['Revenue'])
-    plots_df.plot(ax=axs[3], y=['Earnings before Tax'])
-    plots_df.plot(ax=axs[3], y=['Net Income'])
-    plots_df.plot(ax=axs[3], y=['Free Cash Flow'])
-    plots_df.plot(ax=axs[3], y=['Gross Profit'], grid=True)
+    plots_df.plot(ax=axs[3], y=['Revenue'], lw=4)
+    plots_df.plot(ax=axs[3], y=['Earnings before Tax'], lw=4)
+    plots_df.plot(ax=axs[3], y=['Net Income'], lw=4)
+    plots_df.plot(ax=axs[3], y=['Free Cash Flow'], lw=4)
+    plots_df.plot(ax=axs[3], y=['Gross Profit'], grid=True, lw=4)
 
     axs[3].set_ylabel('Growth')
 
@@ -381,6 +390,180 @@ def plot_basic_charts():
         ax.label_outer()
         ax.minorticks_on()
         ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
+    return linear_trends_df
+
+
+def TrendsPlot(symbol, price_df = ''):
+    set_stock(symbol)
+    if price_df == '':
+        price_df = GetStockDataFrame(get_stock())
+
+    # Get stock company information
+    sp_df = pd.read_csv(sp_cons_csv)
+    stock_info = sp_df[sp_df.Symbol == get_stock().upper()]
+    company_name = stock_info['Name'].iloc[0]
+    company_sector = stock_info['Sector'].iloc[0]
+
+    price_data_df = price_df.copy(deep=True)
+    price_data_df.dropna()
+    dif_period = 5
+
+    # Load stock End of Day prices file
+    price_data_df = GetStockDataFrame(get_stock())
+
+    price_change = 'Price $ Change from ' + str(dif_period) + ' days ago'
+    price_data_df[price_change] = price_data_df['AdjClose'].diff(dif_period).dropna().astype(float)
+
+    price_change_percent = 'Price % Change from ' + str(dif_period) + ' days ago'
+    price_data_df[price_change_percent] = price_data_df['AdjClose'].pct_change(dif_period).dropna().astype(float) * 100
+
+    volume_change_percent = 'Volume % Change from ' + str(dif_period) + ' days ago'
+    price_data_df[volume_change_percent] = price_data_df['Volume'].pct_change(dif_period).dropna().astype(float) * 100
+
+    # ***************************************************
+    # Find the full trend line
+    x = range(len(price_data_df['Timestamps']))
+    fit = np.polyfit(x, price_data_df['AdjClose'].astype(float), 1)
+    fit_fn = np.poly1d(fit)
+    trend = []
+    for k in x:
+        t = fit_fn(k)
+        trend.append(t)
+    price_data_df['5 Yr Trend'] = trend
+    # ***************************************************
+
+    twoyears_ago = datetime.datetime.today() - datetime.timedelta(days=2 * 365)
+    price_df_2y = price_data_df[price_data_df['Timestamps'] > twoyears_ago].copy(deep=True)
+    # Find the 2 years trend line
+    x = range(len(price_df_2y['Timestamps']))
+    fit = np.polyfit(x, price_df_2y['AdjClose'].astype(float), 1)
+    fit_fn = np.poly1d(fit)
+    trend = []
+    for k in x:
+        t = fit_fn(k)
+        trend.append(t)
+    price_df_2y['2 Yr Trend'] = trend
+    # ***************************************************
+
+    oneyears_ago = datetime.datetime.today() - datetime.timedelta(days=1 * 365)
+    price_df_1y = price_data_df[price_data_df['Timestamps'] > oneyears_ago].copy(deep=True)
+    # Find the 1 year trend line
+    x = range(len(price_df_1y['Timestamps']))
+    fit = np.polyfit(x, price_df_1y['AdjClose'].astype(float), 1)
+    fit_fn = np.poly1d(fit)
+    trend = []
+    for k in x:
+        t = fit_fn(k)
+        trend.append(t)
+    price_df_1y['1 Yr Trend'] = trend
+    # ***************************************************
+
+    sixmonth_ago = datetime.datetime.today() - datetime.timedelta(days=0.5 * 365)
+    price_df_6m = price_data_df[price_data_df['Timestamps'] > sixmonth_ago].copy(deep=True)
+    # Find the 6 months trend line
+    x = range(len(price_df_6m['Timestamps']))
+    fit = np.polyfit(x, price_df_6m['AdjClose'].astype(float), 1)
+    fit_fn = np.poly1d(fit)
+    trend = []
+    for k in x:
+        t = fit_fn(k)
+        trend.append(t)
+    price_df_6m['6 Month Trend'] = trend
+    # ***************************************************
+
+    stock_title = stock.upper() + ' ' + '(' + company_name + ')' + ' [' + company_sector + ' Sector]'
+    ts = pd.to_datetime(str(price_data_df['Timestamps'].iloc[0]))
+    frm = ts.strftime('%Y-%m-%d')
+    ts = pd.to_datetime(str(price_data_df['Timestamps'].iloc[-1]))
+    to = ts.strftime('%Y-%m-%d')
+
+    stock_title = stock_title + '\nFrom ' + frm + ' To ' + to
+
+    price_data_df['2 Yr Trend'] = price_df_2y['2 Yr Trend'].copy(deep=True)
+    price_data_df['1 Yr Trend'] = price_df_1y['1 Yr Trend'].copy(deep=True)
+    price_data_df['6 Month Trend'] = price_df_6m['6 Month Trend'].copy(deep=True)
+
+    price_data_df.set_index('Timestamps', inplace=True)
+
+    # , squeeze=True, gridspec_kw = {'height_ratios':[1,2,5,2,3]}
+    fig, axs = plt.subplots(8, figsize=(15, 25), sharex=True,
+                            gridspec_kw={'hspace': 0, 'wspace': 0, 'height_ratios': [3, 3, 3, 3, 2, 2, 2, 2]})
+    fig.suptitle(stock_title, fontsize=30)
+
+    price_data_df.plot(ax=axs[0], y=['Low', 'AdjClose', 'High', '5 Yr Trend'], grid=True)
+    price_data_df.plot(ax=axs[1], y=['Low', 'AdjClose', 'High', '2 Yr Trend'], grid=True)
+    price_data_df.plot(ax=axs[2], y=['Low', 'AdjClose', 'High', '1 Yr Trend'], grid=True)
+    price_data_df.plot(ax=axs[3], y=['Low', 'AdjClose', 'High', '6 Month Trend'], grid=True)
+
+    price_data_df.plot(ax=axs[4], y=[price_change], grid=True, color='green')
+    axs[4].set_ylabel('Price Delta in ' + str(dif_period) + 'd')
+
+    price_data_df.plot(ax=axs[5], y=[price_change_percent], grid=True, color='black')
+    axs[5].set_ylabel('Price % Delta in ' + str(dif_period) + 'd')
+
+    price_data_df.plot(ax=axs[6], y=['Volume'], grid=True, color='green')
+    axs[6].set_ylabel('Vol. Delta in ' + str(dif_period) + 'd')
+
+    price_data_df.plot(ax=axs[7], y=[volume_change_percent], grid=True, color='black')
+    axs[7].set_ylabel('Vol. % Delta in ' + str(dif_period) + 'd')
+
+    # Find Max and Min for price and volume changes and mark price max and min on chart
+    max_price_change = price_data_df[np.round(price_data_df[price_change_percent], 2)
+                                     == np.round(price_data_df[price_change_percent].max(), 2)]
+    min_price_change = price_data_df[np.round(price_data_df[price_change_percent], 2)
+                                     == np.round(price_data_df[price_change_percent].min(), 2)]
+
+    max_vol_change = price_data_df[price_data_df[volume_change_percent]
+                                   == price_data_df[volume_change_percent].max()]
+    min_vol_change = price_data_df[price_data_df[volume_change_percent]
+                                   == price_data_df[volume_change_percent].min()]
+
+    # Hide x labels and tick labels for all but bottom plot.
+    for ax in axs:
+        ax.label_outer()
+        ax.minorticks_on()
+        ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
+    ts = pd.to_datetime(str(max_price_change.index.values[0]))
+    mxp = ts.strftime('%Y-%m-%d')
+
+    ts = pd.to_datetime(str(min_price_change.index.values[0]))
+    mnp = ts.strftime('%Y-%m-%d')
+
+    ts = pd.to_datetime(str(max_vol_change.index.values[0]))
+    mxv = ts.strftime('%Y-%m-%d')
+
+    # Annotate charts with max and min
+    axs[5].annotate(str(np.round(max_price_change[price_change_percent].values[0], 2)) + '%',
+                    xy=(max_price_change.index.values[0],
+                        max_price_change[price_change_percent].values[0]),
+                    xytext=(max_price_change.index.values[0],
+                            max_price_change[price_change_percent].values[0] * 0.5),
+                    arrowprops=dict(facecolor='red', shrink=0.05),
+                    )
+
+    ts = pd.to_datetime(str(min_vol_change.index.values[0]))
+    mnv = ts.strftime('%Y-%m-%d')
+    axs[5].annotate(str(np.round(min_price_change[price_change_percent].values[0], 2)) + '%',
+                    xy=(min_price_change.index.values[0],
+                        min_price_change[price_change_percent].values[0]),
+                    xytext=(min_price_change.index.values[0],
+                            min_price_change[price_change_percent].values[0] * 0.5),
+                    arrowprops=dict(facecolor='red', shrink=0.05),
+                    )
+
+    maxtable_df = pd.DataFrame({'Change Type': ['Max % Rise', 'Max % Drop', 'Max % Rise', 'Max % Drop'],
+                                'Date': [mxp, mnp, mxv, mnv],
+                                'Price': [str(np.round(max_price_change[price_change_percent].values[0], 2)) + '%',
+                                          str(np.round(min_price_change[price_change_percent].values[0], 2)) + '%',
+                                          '--', '--'],
+                                'Volume': ['--', '--',
+                                           str(np.round(max_vol_change[volume_change_percent].values[0], 2)) + '%',
+                                           str(np.round(min_vol_change[volume_change_percent].values[0], 2)) + '%']})
+    maxtable_df = maxtable_df.set_index('Change Type')
+
+    return price_data_df, maxtable_df
 
 def quote(symbol):
     set_stock(symbol)
@@ -399,10 +582,10 @@ def key_stat(symbol):
     set_stock(symbol)
 
     # Get stock company information
-    sp_df = pd.read_csv(sp_cons_csv)
-    stock_info = sp_df[sp_df.Symbol == stock.upper()]
-    company_name = stock_info['Name'].iloc[0]
-    company_sector = stock_info['Sector'].iloc[0]
+    stock_info = get_stock_info(symbol)
+    # Get stock company information
+    company_name = stock_info['name'][0]
+    company_sector = stock_info['sector'][0]
 
     # Load the stock fundamental data
     keys, column_map, main_df_org = GetFund_Dict(stock)
--- test_data.py
+++ test_data.py
@@ -1,174 +1,80 @@
-
+#!/usr/bin/env python
+# coding: utf-8
 import stockdatalib as sd
-from IPython.display import display
-from IPython.display import HTML
-import pandas as pd
-import datetime as datetime
-import numpy as np
-import matplotlib.pyplot as plt
 
-def plot_stock_charts():
-    stock_title = sd.stock.upper() + ' ' + '(' + company_name + ')' + ' [' + company_sector + ' Sector]'
-    price_df = sd.GetStockDataFrame(sd.stock)
-    mean_1 = price_df['AdjClose'].rolling(window=100).mean()
-    mean_1_name = '100d Mov.Ave.'
-    mean_1_df = pd.DataFrame({'Timestamps': price_df['Timestamps'].to_list(), mean_1_name: mean_1})
-    price_df = pd.merge(price_df, mean_1_df, on='Timestamps')
-
-    fig = plt.figure(figsize=(15, 6))
-
-    # ***************************************************
-    # Find the full trend line
-    x = range(len(price_df['Timestamps']))
-    fit = np.polyfit(x, price_df['AdjClose'].astype(float), 1)
-    fit_fn = np.poly1d(fit)
-    print(fit_fn)
-
-    trend = []
-    for k in x:
-        t = fit_fn(k)
-        trend.append(t)
-    price_df['5 Yr Trend'] = trend
-    # ***************************************************
-
-    twoyears_ago = datetime.datetime.today() - datetime.timedelta(days=2 * 365)
-    price_df_2y = price_df[price_df['Timestamps'] > twoyears_ago].copy(deep=True)
-    # price_df = price_df_2y
-    # Find the 2 years trend line
-    x = range(len(price_df_2y['Timestamps']))
-    fit = np.polyfit(x, price_df_2y['AdjClose'].astype(float), 1)
-    fit_fn = np.poly1d(fit)
-    print(fit_fn)
-
-    trend = []
-    for k in x:
-        t = fit_fn(k)
-        trend.append(t)
-    price_df_2y['2 Yr Trend'] = trend
-    # ***************************************************
-
-    oneyears_ago = datetime.datetime.today() - datetime.timedelta(days=1 * 365)
-    price_df_1y = price_df[price_df['Timestamps'] > oneyears_ago].copy(deep=True)
-    # Find the 1 year trend line
-    x = range(len(price_df_1y['Timestamps']))
-    fit = np.polyfit(x, price_df_1y['AdjClose'].astype(float), 1)
-    fit_fn = np.poly1d(fit)
-    print(fit_fn)
-
-    trend = []
-    for k in x:
-        t = fit_fn(k)
-        trend.append(t)
-    price_df_1y['1 Yr Trend'] = trend
-    # ***************************************************
-
-    sixmonth_ago = datetime.datetime.today() - datetime.timedelta(days=0.5 * 365)
-    price_df_6m = price_df[price_df['Timestamps'] > sixmonth_ago].copy(deep=True)
-    # Find the 6 months trend line
-    x = range(len(price_df_6m['Timestamps']))
-    fit = np.polyfit(x, price_df_6m['AdjClose'].astype(float), 1)
-    fit_fn = np.poly1d(fit)
-    print(fit_fn)
-
-    trend = []
-    for k in x:
-        t = fit_fn(k)
-        trend.append(t)
-    price_df_6m['6 Month Trend'] = trend
-    # ***************************************************
-
-    ts = pd.to_datetime(str(price_df['Timestamps'].iloc[0]))
-    frm = ts.strftime('%Y-%m-%d')
-    ts = pd.to_datetime(str(price_df['Timestamps'].iloc[-1]))
-    to = ts.strftime('%Y-%m-%d')
-
-    stock_title = stock_title + '\nFrom ' + frm \
-                  + ' To ' + to
-
-    fig, axs = plt.subplots(3, figsize=(15, 20), sharex=True, gridspec_kw={'hspace': 0, 'wspace': 0})
-    fig.suptitle(stock_title, fontsize=30)
-
-    price_df.plot(ax=axs[0], x='Timestamps', y=['Low', 'AdjClose', mean_1_name, 'High', '5 Yr Trend'],
-                  title=stock_title)
-    price_df_2y.plot(ax=axs[0], x='Timestamps', y=['2 Yr Trend'])
-    price_df_1y.plot(ax=axs[0], x='Timestamps', y=['1 Yr Trend'])
-    price_df_6m.plot(ax=axs[0], x='Timestamps', y=['6 Month Trend'], grid=True)
-
-    # earn_df =
-
-    dic = [{}]
-    p_over_e_df = [{}]  # pd.DataFrame(columns=['Timestamps','Price/Earning'])
-
-    i = 0
-    main_df['EPS'] = sd.MakeFund_Subset(main_df_org['income-statement'], ['EPS'])
-    main_df['PE ratio'] = sd.MakeFund_Subset(main_df_org['company-key-metrics'], ['PE ratio'])
-
-    from_date = main_df['EPS'].index[i]
-
-    to_date = main_df['EPS'].index[i + 1]
-    for d in price_df['Timestamps']:
-        if d > to_date:
-            i = i + 1
-            if i < len(main_df['EPS']) - 1:
-                from_date = main_df['EPS'].index[i]
-                to_date = main_df['EPS'].index[i + 1]
-                continue
-            else:
-                break
-
-        elif from_date <= d <= to_date:
-            jj = main_df[main_df.index == from_date]['EPS'][0]
-            pp = main_df[main_df.index == from_date]['PE ratio'][0]
-
-            kk = price_df[price_df['Timestamps'] == d]['AdjClose'].values[0]
-
-            dic.append({'Timestamps': d, 'Calc. P/E': (kk / jj) / 4.0, 'EPS': jj, 'P/E': pp})
-
-        else:
-            continue
-
-    p_over_e_df = pd.DataFrame(dic).set_index('Timestamps').dropna()
-
-    p_over_e_df.plot(ax=axs[1], y=['EPS'], grid=True)
-    p_over_e_df.plot(ax=axs[2], y=['P/E'], grid=True)
-    p_over_e_df.plot(ax=axs[2], y=['Calc. P/E'], grid=True)
-
-
+# # Must set the data subdirectory
 # Set the data directory and the stock name
 sd.set_data_directory('./data/')
-sd.set_stock('aapl')
 
+
+# # Load Data and plot a stock
 # Load S&P Stock list
 stock_count, stock_fields, sp_df = sd.GetSP500_List()
 
-# Get stock company information
-stock_info = sp_df[sp_df.Symbol == sd.stock.upper()]
-company_name = stock_info['Name'].iloc[0]
-company_sector = stock_info['Sector'].iloc[0]
+# Set stock symbol
+sd.set_stock('jpm')
 
+stock_info = sd.get_stock_info('jpm')
+# Get stock company information
+company_name = stock_info['name'][0]
+company_sector = stock_info['sector'][0]
+
+# Plot stock basic charts
+linear_trends = sd.plot_basic_charts('jpm')
+
+# Linear Regression Equations returned by the plot call
+linear_trends
+
+# Get last price quote
+sd.quote('csco')
+
+# Get last key stats on the stock
+sd.key_stat('ibm')
+
+# Plot another stock
+sd.plot_basic_charts('amzn')
+
+# # The data load functions
 
 # Load stock End of Day prices file
-price_df = sd.GetStockDataFrame(sd.stock)
+price_df = sd.GetStockDataFrame('fb')
+
 # Index the data frame by 'Timestamps' column
 price_df.set_index('Timestamps',inplace=True)
 
 # Load the stock fundamental data
-keys, column_map, main_df_org = sd.GetFund_Dict(sd.stock)
+keys, column_map, main_df_org = sd.GetFund_Dict('fb')
 
+
+# # Searching for data by column name
 # Search for a key word in the data column names
-f,s = sd.where_is_column('Revenue')
-display(f.to_html())
+table,text = sd.FindColumn('Revenue')
+table
+
+# Now we know where 'Capex to Revenue' is, we can load it from the DataFrame source 'company-key-metrics' as 
+# the code below
+main_df_org['company-key-metrics']['Capex to Revenue'].plot(figsize=(12,8),
+                                                            grid=True, 
+                                                            title=sd.get_stock().upper()+' Capex to Revenue')
+
+
+# # Selecting data range from price data frame
 
 # Limit the stock prices data in the dates range
-price_df = sd.df_start_after_datetime(price_df, '2015-01-01','2016-06-01')
+price_df = sd.DatesRange(price_df, '2016-01-01', '2018-06-01')
 
 # Plot the prices chart
-#price_df['AdjClose'].plot(figsize=(15,8)).grid()
+ax1 = price_df['AdjClose'].plot(figsize=(15,8),
+                                title=sd.get_stock().upper()+" Plot selected date range").grid()
+
+
+# # Merging DataFrames from multiple sources into one DF
+import pandas as pd
 
 # Create a subset of columns in a DataFrame from the stock fundamentals dataset
 cols = ['Revenue','Earnings before Tax','Net Income','Free Cash Flow margin','Gross Profit','Net Profit Margin'] # [ 'Shares', 'Price', 'Revenue', 'Earnings']
 main_df1 = sd.MakeFund_Subset(main_df_org['income-statement'],cols)
-main_df1.plot()
+
 # Create another subset of columns from another fundamentals dataset
 cols = ['Number of Shares','Stock Price']
 main_df2 = sd.MakeFund_Subset(main_df_org['enterprise-value'],cols)
@@ -177,13 +83,38 @@ main_df2 = sd.MakeFund_Subset(main_df_org['enterprise-value'],cols)
 cols = ['Capital Expenditure','Free Cash Flow']
 main_df3 = sd.MakeFund_Subset(main_df_org['cash-flow-statement'],cols)
 
+# Create a 4th subject of columns from yet another fundamentals dataset
+cols = ['Free Cash Flow per Share']
+main_df4 = sd.MakeFund_Subset(main_df_org['company-key-metrics'],cols)
+
+
 # Merge the 3 dataframes into a single dataframe
 main_df = pd.merge(main_df1,main_df2,on='date')
 main_df = pd.merge(main_df,main_df3,on='date')
+main_df = pd.merge(main_df,main_df4,on='date')
 
-#main_df.plot()
-main_df['Net Income'].plot().grid()
-main_df.plot(y=['Revenue','Earnings before Tax','Net Income','Free Cash Flow','Gross Profit'],figsize=(15,8)).grid()
+main_df.plot(y=['Revenue','Earnings before Tax','Net Income','Free Cash Flow','Gross Profit'],
+             figsize=(15,8),title=sd.get_stock().upper()).grid()
 
 
-plot_stock_charts()
+# # Using DatesRange() on fundamental data
+sd.DatesRange(main_df, '2016-01-01', '2019-06-01').plot(figsize=(12,8),
+                                                                     grid=True,
+                                                                     title=sd.get_stock().upper()+' From 2016-01-01 TO 2019-06-01')
+
+# Plot price chart with trend lines, EPS, and PE ratios
+sd.plot_basic_charts('nflx')
+
+
+# # Plot a single key stat item
+main_df['Net Income'].plot(figsize=(12,8), title = sd.get_stock().upper()).grid()
+
+price_data_df, maxtable_df = sd.TrendsPlot('c')
+
+maxtable_df
+
+price_data_df.describe()
+
+f,s = sd.FindColumn('cash flow')
+f
+
